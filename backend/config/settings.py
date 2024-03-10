8b0980e93efaba3fffa5883051432a3f52d2fced
aLi <sabeti.py@gmail.com>
2024-03-10 16:39:02 +0330

    Changing the database to postgresql

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': int(os.getenv("DB_PORT")),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASS"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
