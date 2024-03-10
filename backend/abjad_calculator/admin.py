bfa34dcfb9148e5a2ab04a39a6b9f4bc2b2cc092
aLi <sabeti.py@gmail.com>
2024-03-10 16:24:13 +0330

    Add model classes

--- backend/abjad_calculator/admin.py
+++ backend/abjad_calculator/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import PersianWord, EnglishWord
 
-# Register your models here.
+class PersianWordAdmin(admin.ModelAdmin):
+	list_display = ('word', 'kabir_abjad', 'saghir_abjad', 'vasit_abjad', 'search', 'updated_at', 'created_at')
+	search_fields = ('word', )
+admin.site.register(PersianWord, PersianWordAdmin)
+
+
+class EnglishWordAdmin(admin.ModelAdmin):
+	list_display = ('word', 'abjad_hebrew', 'abjad_english', 'abjad_simple', 'search', 'updated_at', 'created_at')
+	search_fields = ('word', )
+
+admin.site.register(EnglishWord, EnglishWordAdmin)
--- backend/abjad_calculator/models.py
+++ backend/abjad_calculator/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class PersianWord(models.Model):
+	word = models.CharField(max_length=200)
+	kabir_abjad = models.IntegerField(default=0)
+	saghir_abjad = models.IntegerField(default=0)
+	vasit_abjad = models.IntegerField(default=0)
+	search = models.IntegerField(default=0)
+
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.word
+
+class EnglishWord(models.Model):
+	word = models.CharField(max_length=200)
+	abjad_hebrew = models.IntegerField(default=0)
+	abjad_english = models.IntegerField(default=0)
+	abjad_simple = models.IntegerField(default=0)
+	search = models.IntegerField(default=0)
+
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.word
