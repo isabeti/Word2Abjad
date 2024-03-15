c5ff9fb9e5e589a897c65dc7ad86b3ae670e02dc
aLi <sabeti.py@gmail.com>
2024-03-15 17:57:37 +0330

    add forms and dictionary file

--- backend/abjad_calculator/views.py
+++ backend/abjad_calculator/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+
+from .forms import AbjadCalculateForm
+from . import dictionary
+from .utils import abjad_clalc_FA, abjad_clalc_EN
 
 # Create your views here.
+class AbjadCalculateFA(APIView):
+    def post(slef, request):
+        form = AbjadCalculateForm(request.data)
+        if form.is_valid():
+            word = form.cleaned_data['word']
+            is_persian = form.cleaned_data['is_persian']
+
+            if is_persian == True:
+                data = abjad_clalc_FA(word)
+            else:
+                data = abjad_clalc_EN(word)
+            return Response(data)
+        return Response(form.errors)
+    
