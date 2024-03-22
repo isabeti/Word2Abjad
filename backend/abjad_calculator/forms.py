eb3cd2b2ea1c3f45b01ef07c3d5b90cb0b0f1301
aLi <sabeti.py@gmail.com>
2024-03-22 20:06:50 +0330

    update API(fix bug)

--- backend/abjad_calculator/forms.py
+++ backend/abjad_calculator/forms.py
@@ -8,17 +8,16 @@ class AbjadCalculateForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         word = cleaned_data['word']
-        word_without_space = word.replace(' ', '')
 
         error_fa = False
         error_en = False
-        for i in word_without_space:
+        for i in word:
             if i not in dictionary.kabir_abjad or i not in dictionary.saghir_abjad or i not in dictionary.vasit_abjad:
                 error_fa = True
                 break
             cleaned_data['is_persian'] = True
 
-        for i in word_without_space:
+        for i in word:
             if i not in dictionary.abjad_hebrew or i not in dictionary.abjad_english or i not in dictionary.abjad_simple:
                 error_en = True
                 break
--- backend/abjad_calculator/utils.py
+++ backend/abjad_calculator/utils.py
@@ -3,6 +3,7 @@ from .models import PersianWord, EnglishWord
 from . import serializers
 
 def abjad_clalc_FA(word):
+    new_word = ''
     kabir_letters = []
     list_kabir_number = []
     kabir_number = 0
@@ -15,7 +16,11 @@ def abjad_clalc_FA(word):
     list_vasit_number = []
     vasit_number = 0
     
-    for i in word.replace(' ', ''):
+    for i in word:
+        if len(kabir_letters) != 0 and i == " " and kabir_letters[-1] == i:
+            continue
+
+        new_word += i
         kabir_letters.append(i)
         saghit_letters.append(i)
         vasit_letters.append(i)
@@ -29,14 +34,13 @@ def abjad_clalc_FA(word):
         vasit_number += dictionary.vasit_abjad[i]
 
     box_data = {
-        'kabir': {'word': word, 'kabir_number': kabir_number, 'letters': kabir_letters, 'list_kabir_number': list_kabir_number},
-        'saghit': {'word': word, 'saghit_number': saghit_number, 'letters': saghit_letters, 'list_saghit_number': list_saghit_number},
-        'vasit': {'word': word, 'vasit_number': vasit_number, 'letters': vasit_letters, 'list_vasit_number': list_vasit_number},
+        'kabir': {'word': new_word, 'kabir_number': kabir_number, 'letters': kabir_letters, 'list_kabir_number': list_kabir_number},
+        'saghit': {'word': new_word, 'saghit_number': saghit_number, 'letters': saghit_letters, 'list_saghit_number': list_saghit_number},
+        'vasit': {'word': new_word, 'vasit_number': vasit_number, 'letters': vasit_letters, 'list_vasit_number': list_vasit_number},
     }
 
-
     obj, create = PersianWord.objects.get_or_create(
-        word=word.strip(), kabir_abjad=kabir_number, saghir_abjad=saghit_number, vasit_abjad=vasit_number)
+        word=new_word, kabir_abjad=kabir_number, saghir_abjad=saghit_number, vasit_abjad=vasit_number)
 
     obj.search += 1
     obj.save()
@@ -44,10 +48,11 @@ def abjad_clalc_FA(word):
     objects = PersianWord.objects.filter(kabir_abjad=obj.kabir_abjad)
     table_data = serializers.PersianWordSerilaizer(objects, many=True).data
 
-    return {'box_data': box_data, 'table_data': table_data}
+    return {'input': new_word, 'box_data': box_data, 'table_data': table_data}
 
 
 def abjad_clalc_EN(word):
+    new_word = ''
     hebrew_letters = []
     list_hebrew_numbers = []
     hebrew_number = 0
@@ -60,7 +65,11 @@ def abjad_clalc_EN(word):
     list_simple_numbers = []
     simple_number = 0
     
-    for i in word.replace(' ', ''):
+    for i in word:
+        if len(hebrew_letters) != 0 and i == " " and hebrew_letters[-1] == i:
+            continue
+
+        new_word += i
         hebrew_letters.append(i)
         english_letters.append(i)
         simple_letters.append(i)
@@ -74,14 +83,13 @@ def abjad_clalc_EN(word):
         simple_number += dictionary.abjad_simple[i]
 
     box_data = {
-        'hebrew': {'word': word, 'hebrew_number': hebrew_number, 'letters': hebrew_letters, 'list_hebrew_numbers': list_hebrew_numbers},
-        'english': {'word': word, 'english_number': english_number, 'letters': english_letters, 'list_english_numbers': list_english_numbers},
-        'simple': {'word': word, 'simple_number': simple_number, 'letters': simple_letters, 'list_simple_numbers': list_simple_numbers},
+        'hebrew': {'word': new_word, 'hebrew_number': hebrew_number, 'letters': hebrew_letters, 'list_hebrew_numbers': list_hebrew_numbers},
+        'english': {'word': new_word, 'english_number': english_number, 'letters': english_letters, 'list_english_numbers': list_english_numbers},
+        'simple': {'word': new_word, 'simple_number': simple_number, 'letters': simple_letters, 'list_simple_numbers': list_simple_numbers},
     }
 
-
     obj, create = EnglishWord.objects.get_or_create(
-        word=word.strip(), abjad_hebrew=hebrew_number, abjad_english=english_number, abjad_simple=simple_number)
+        word=new_word, abjad_hebrew=hebrew_number, abjad_english=english_number, abjad_simple=simple_number)
 
     obj.search += 1
     obj.save()
@@ -89,5 +97,5 @@ def abjad_clalc_EN(word):
     objects = EnglishWord.objects.filter(abjad_hebrew=obj.abjad_hebrew)
     table_data = serializers.EnglishWordSerilaizer(objects, many=True).data
 
-    return {'box_data': box_data, 'table_data': table_data}
+    return {'input': new_word, 'box_data': box_data, 'table_data': table_data}
 
